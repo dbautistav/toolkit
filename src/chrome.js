92b3c6d9ea901dd9e31ad2f7d036020cf46875a8
David Bautista <dbautistav@gmail.com>
2016-03-31 23:38:51 -0600

    Messages to js console // Notes

--- src/chrome.js
+++ src/chrome.js
@@ -5,7 +5,10 @@ import "babel-polyfill";
 
 export function getTabsByBrowserActionOnClickListener() {
     return new Promise((resolve) => {
-        chrome.browserAction.onClicked.addListener((tabs) => { resolve(tabs); });
+        chrome.browserAction.onClicked.addListener((tabs) => {
+            console.log("tabs", tabs);
+            resolve(tabs);
+        });
     });
 }
 export const browserActionOnClickedAddListener = getTabsByBrowserActionOnClickListener;
@@ -13,6 +16,7 @@ export const browserActionOnClickedAddListener = getTabsByBrowserActionOnClickLi
 
 export function sendRuntimeMessage(message) {
     return new Promise((resolve) => {
+        console.log("message", message);
         chrome.runtime.sendMessage(undefined, message, undefined, () => { resolve(true); });
     });
 }
@@ -21,7 +25,15 @@ export const runtimeSendMessage = sendRuntimeMessage;
 
 export function getResponseByRuntimeOnMessageListener() {
     return new Promise((resolve) => {
-        chrome.runtime.onMessage.addListener((response) => { resolve(response); });
+        chrome.runtime.onMessage.addListener((response) => {
+            console.log("response", response);
+            resolve(response);
+            console.log("response (after 'resolve(response);')", response, resolve);
+
+            chrome.runtime.getBackgroundPage((_win) => {
+                _win.alert(response);
+            });
+        });
         //(response, sender, sendResponse) => {
         //    alert(response);
         //}
